40df8fc15e65f820963c878cf76743939a54ea54
notes_514 <851559442@qq.com>
2021-06-09 10:03:45 +0800

    初始提交（整合前后端）

---
+++ teaching-be/src/main/java/com/guidian/teaching/TeachingApplication.java
@@ -0,0 +1,18 @@
+package com.guidian.teaching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description SpringBoot应用入口
+ * @author dhxstart
+ * @date 2021/6/9 9:56
+ */
+@SpringBootApplication
+public class TeachingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeachingApplication.class, args);
+    }
+
+}
---
+++ teaching-be/src/test/java/com/guidian/teaching/TeachingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guidian.teaching;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeachingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
